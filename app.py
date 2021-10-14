0439ae31358c3ce13dc8e5f13fe6660050c70ec6
Trevor <wtrevor242@gmail.com>
2021-10-13 18:07:33 -0600

    Final submission backend

--- app.py
+++ app.py
@@ -35,13 +35,18 @@ def getPosts():
     
     try:
         (conn, cursor) = connectDB()
-        cursor.execute("SELECT content FROM posts")
+        cursor.execute("SELECT * FROM posts")
         list = cursor.fetchall()
         content_list = []
         content = {}
         for result in list:
-            content = {'content': result[0],
-                        }
+            created_at = result[2]
+            created_at_serialized = created_at.strftime("%Y-%m-%d")
+            content = {
+                "id": result[0],
+                "content": result[1],
+                "created_at": created_at_serialized
+                }
             content_list.append(content)
 
         return Response(json.dumps(content_list), mimetype="application/json", status=200)
@@ -65,15 +70,20 @@ def createPost():
     try:
         (conn, cursor) = connectDB()
         data = request.json
+        
+        from datetime import datetime
+        cur_date = datetime.today().strftime('%Y-%m-%d')
         client_content = data.get('content')
 
         if (client_content is not None):    
+            
             resp = {
-                "content": client_content
+                "content": client_content,
             }
-            cursor.execute("INSERT INTO posts(content) VALUES(?)", [client_content])
+            cursor.execute("INSERT INTO posts (content, created_at) VALUES(?,?)", [client_content,cur_date])
             conn.commit()
         return Response(json.dumps(resp), mimetype="application/json", status=200)
+    
     except ConnectionError:
         print("Error while attempting to connect to the database")
     except mariadb.DataError:
@@ -96,7 +106,7 @@ def updatePost():
         data = request.json
         client_id = data.get('id')
         client_content = data.get('content')
-  
+
         if (client_content is not None):
             
             resp = {
@@ -172,9 +182,11 @@ def blogPostsApi():
 if (len(sys.argv) > 1):
     mode = sys.argv[1]
     if (mode == "production"):
+        from flask_cors import CORS
+        CORS(app)
         import bjoern
         host = '0.0.0.0'
-        port = 5000
+        port = 5005
         print("Server is running in production mode")
         bjoern.run(app, host, port)
     elif (mode == "testing"):
